3c0c59d992725276f5e7dea1ccc19e70b4c804cf
Kien Lam <kien.lam@gbst.com>
2023-08-09 18:40:09 +0700

    Nothing changes

--- Controller.py
+++ Controller.py
@@ -7,11 +7,12 @@ import random
 class Controller():
     def __init__(self,gui):
         self.gui = gui
-        self.collectionFileName = gui.get_collection_file()
+        self.collectionFileName = gui.get_collection_files()
         self.environmentFiles = gui.get_environment_files()
         self.iterationTime = gui.get_iteration_run()
-        self.useableEnvironmentFile = []
-        self.notUsableEnvironmentFile = []
+        self.runParallel = gui.get_run_parralel_check_button_value()
+        self.usableEnvironmentFiles = []
+        self.notUsableEnvironmentFiles = []
     
     def run_process(self):
         try:
@@ -24,12 +25,12 @@ class Controller():
                 dialogType = "Info"
                 title = "Confirm before start process"
                 message = "The environment file that will be run on process:\n"
-                for environment in self.useableEnvironmentFile:
+                for environment in self.usableEnvironmentFiles:
                     message = message + environment + "\n"
-                if len(self.notUsableEnvironmentFile) > 0:
+                if len(self.notUsableEnvironmentFiles) > 0:
                     message = message + "\nThe environment file that will not be run on process:\n"
-                for environment in self.notUsableEnvironmentFile:
-                    message = message + environment + "\n"
+                    for environment in self.notUsableEnvironmentFiles:
+                        message = message + environment + "\n"
                 result = self.display_information_dialog(dialogType,title,message)
                 if result == True:
                     self.start()
@@ -42,10 +43,10 @@ class Controller():
 
         
     def verify_collection_file(self):
-        if(len(self.collectionFileName) == 0):
+        if(len(self.collectionFileName) == 1):
             raise Exception("Error","Collection file validation", "The collection file could not be empty, please retry!!!")
         else:
-            return
+            self.collectionFileName = self.collectionFileName.split("\n")
         
     def verify_environment_file(self):
         if(len(self.environmentFiles) == 1):
@@ -56,15 +57,14 @@ class Controller():
             
     def check_environment_files(self, environmentFiles):
         for environmentFile in environmentFiles:
-            if(environmentFile != "\n"):
+            if(environmentFile != "\n" and environmentFile != ""):
                 trimmedEnvironmentFile = environmentFile.strip()
                 if(os.path.isfile(trimmedEnvironmentFile)):
-                    self.useableEnvironmentFile.append(trimmedEnvironmentFile)
+                    self.usableEnvironmentFiles.append(trimmedEnvironmentFile)
                 else:
-                    self.notUsableEnvironmentFile.append(trimmedEnvironmentFile)
+                    self.notUsableEnvironmentFiles.append(trimmedEnvironmentFile)
             else:
                 pass
-                
             
     def verify_iteration_run_time(self):
         try:
@@ -89,14 +89,40 @@ class Controller():
         dialogType = "Success"
         title = "Process run result"
         message = "The process was run successfully and the run report saved into the report folder, please check the folder to get the run report !!!"
-        if(len(self.environmentFiles) == 1):
-            process = subprocess.Popen(["node", processFile, self.collectionFileName, str(self.iterationTime)])
-            progressBar.animated_run(random.randint(50,90),100)
-            process.wait()
-        else:
-            environmentFileList = ",".join(self.useableEnvironmentFile)
-            process = subprocess.Popen(["node", processFile, self.collectionFileName, environmentFileList, str(self.iterationTime)])
-            progressBar.animated_run(random.randint(50,90),100)
-            process.wait()
+        command = self.get_command_base_on_inputted_data()
+        process = subprocess.Popen(["node",processFile,command])
+        progressBar.animated_run(random.randint(50,90),100)
+        process.wait()
         progressBar.animated_run(100,50)
         self.display_information_dialog(dialogType,title,message)
+        
+    def get_command_base_on_inputted_data(self):
+        collectionString = ""
+        environmentString = ""
+        iterationTime = str(self.iterationTime)
+        command = " "
+        if(len(self.collectionFileName)> 1):
+            collectionString = ",".join(self.collectionFileName)
+        else:
+            collectionString = collectionString + self.collectionFileName[0]
+            
+        if(len(self.usableEnvironmentFiles)> 1):
+            environmentString = ",".join(self.usableEnvironmentFiles)
+        elif(len(self.usableEnvironmentFiles) == 1):
+            environmentString = environmentString + self.usableEnvironmentFiles[0]
+        else:
+            pass
+        
+        if(environmentString == ""):
+            command = command + "-c " + '"' + collectionString  + '"' + " -n " + iterationTime
+        else:
+            command = command + "-c " + '"' + collectionString + '"' + " -e " + '"' + environmentString + '"' + " -n " + iterationTime
+            
+        if(self.runParallel == True):
+            command = command + " -p"
+        else:
+            pass
+        
+        print(command)
+            
+        return command
